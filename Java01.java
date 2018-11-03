2b75d3fb82983f3025e34b5d785145e3bba7fdf5
aishangmianbao <2517380713@qq.com>
2018-11-03 21:54:37 +0800

    add Java01.java

---
+++ Java01.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.text.*;
+
+inteface IMessage{
+	public void getMessage();
+}
+inteface IMessage{
+	public void getMessage();
+}
+public class Java01{
+	public static void main(String[] args) throws Exception{
+		IMessage tmp = MessageUtil.getMessage().get();
+		MessageUtil.useMessage(tmp);
+	}	
+}
